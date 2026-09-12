7f1f000c03a95939e28750de256da4688a4beb78
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-12 19:18:53 +0200

    Some grahpics test init

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
-#include <windows.h>
+#include "CubeRenderer.h"
+
+#include <vector>
+
+using namespace CubeRenderer;
+
+static Graphics g_graphics;
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch (msg)
     {
+    case WM_SIZE:
+        g_graphics.Resize(hwnd);
+        return 0;
     case WM_DESTROY:
         PostQuitMessage(0);
         return 0;
@@ -11,9 +20,45 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
     return DefWindowProc(hwnd, msg, wParam, lParam);
 }
 
+Texture* CreateCheckerTexture(ID3D11Device* device)
+{
+    const UINT size = 64;
+    const UINT cell = 8;
+
+    std::vector<BYTE> pixels((size_t)size * size * 4);
+    for (UINT y = 0; y < size; ++y)
+        for (UINT x = 0; x < size; ++x)
+        {
+            bool dark = ((x / cell) + (y / cell)) % 2 == 0;
+            BYTE v = dark ? 30 : 225;
+            BYTE* p = &pixels[((size_t)y * size + x) * 4];
+            p[0] = v;
+            p[1] = v;
+            p[2] = v;
+            p[3] = 255;
+        }
+
+    D3D11_TEXTURE2D_DESC desc = {};
+    desc.Width = size;
+    desc.Height = size;
+    desc.MipLevels = 1;
+    desc.ArraySize = 1;
+    desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    desc.SampleDesc.Count = 1;
+    desc.Usage = D3D11_USAGE_DEFAULT;
+    desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+
+    D3D11_SUBRESOURCE_DATA data = {};
+    data.pSysMem = pixels.data();
+    data.SysMemPitch = size * 4;
+
+    ID3D11Texture2D* texture = nullptr;
+    device->CreateTexture2D(&desc, &data, &texture);
+    return new Texture(texture);
+}
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nShowCmd)
 {
-
     WNDCLASSEXW wc = {};
     wc.cbSize = sizeof(wc);
     wc.lpfnWndProc = WndProc;
@@ -31,11 +76,27 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nShowCmd)
     ShowWindow(hwnd, nShowCmd);
     UpdateWindow(hwnd);
 
-    MSG msg = {};
-    while (GetMessageW(&msg, nullptr, 0, 0) > 0)
+    Scene* scene = g_graphics.Init(hwnd);
+
+    Texture* texture = CreateCheckerTexture(g_graphics.GetDevice());
+    scene->SetTexture(texture);
+    scene->AddCube(16.0f, 16.0f, 16.0f, 0, 0, 0, 0, 0, texture);
+    g_graphics.UpdateScene();
+
+    float angle = 0.0f;
+    for (;;)
     {
-        TranslateMessage(&msg);
-        DispatchMessageW(&msg);
+        MSG msg;
+        while (PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+                return (int)msg.wParam;
+            TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        }
+
+        g_graphics.Render(angle, 0.0f, 0.0f, 0.0f);
+        angle += 0.01f;
+        Sleep(16);
     }
-    return (int)msg.wParam;
 }
