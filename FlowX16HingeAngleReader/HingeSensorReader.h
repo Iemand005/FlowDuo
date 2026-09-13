67122c44af573436c7a45d18eb48908184bbb50e
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-13 06:19:21 +0200

    fix release and debug uh includes by sharing them

---
+++ FlowX16HingeAngleReader/HingeSensorReader.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+#include <Windows.h>
+
+#include <sensorsapi.h>
+#include <sensors.h>
+#pragma comment(lib, "sensorsapi.lib")
+
+#include "em.h"
+
+
+class HingeSensorReader
+{
+private:
+    ISensorManager* pSensorManager = NULL;
+    ISensorCollection* pSensorCollection = NULL;
+    ISensor* pSensor = NULL;
+    IPortableDeviceKeyCollection* pDataFields = NULL;
+
+    PROPERTYKEY pAngleKey, pLidAngleKey, pBodyAngleKey;
+
+    AccelerometerReader accelerometerReader;
+
+    void ThrowIfFailed(HRESULT hr) {
+        if (FAILED(hr)) throw hr;
+    }
+
+public:
+    HingeSensorReader();
+    ~HingeSensorReader();
+
+    HRESULT Init();
+
+    bool IsReady() const;
+
+    HRESULT GetHingeAngle(int* angle, int* lidAngle, int* bodyAngle);
+    HRESULT GetHingeAngleFloat(float* angle);
+
+    bool useRawAccelerometer = false;
+};
