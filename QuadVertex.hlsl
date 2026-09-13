32d92575d6c7c82e499160cfb3f0e969956a345e
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-13 20:34:40 +0200

    shadr

--- QuadVertex.hlsl
+++ QuadVertex.hlsl
@@ -7,18 +7,21 @@ struct VSIn
 {
     float3 pos : POSITION;
     float2 uv : TEXCOORD0;
+    float blur : TEXCOORD1;
 };
 
 struct VSOut
 {
     float4 pos : SV_POSITION;
-    float3 worldPosition : TEXCOORD1;
+    float2 uv : TEXCOORD0;
+    float blur : TEXCOORD1;
 };
 
 VSOut main(VSIn i)
 {
     VSOut o;
     o.pos = mul(float4(i.pos, 1.0), transform);
-    o.worldPosition = i.pos;
+    o.uv = i.uv;
+    o.blur = i.blur;
     return o;
 }
