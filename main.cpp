b8fde923ea832d82a3e631843f89bf4848228b61
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-13 05:44:48 +0200

    lower FOV? you reckon

--- main.cpp
+++ main.cpp
@@ -390,7 +390,8 @@ static void PresentQuad(ID3D11Device* device, ID3D11DeviceContext* context, IDXG
         XMMatrixTranslation(0.0f, -(scale - 1.0f) * g_quadHalfHeight, 0.0f);
     XMMATRIX view = XMMatrixLookAtLH(XMVectorSet(0.0f, 0.0f, -3.0f, 1.0f),
                                      XMVectorZero(), XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f));
-    XMMATRIX proj = XMMatrixPerspectiveFovLH(XMConvertToRadians(60.0f), aspect, 0.1f, 100.0f);
+    float fov = 30.0f
+    XMMATRIX proj = XMMatrixPerspectiveFovLH(XMConvertToRadians(fov), aspect, 0.1f, 100.0f);
     XMMATRIX transform = XMMatrixTranspose(world * view * proj);
 
     const float clear[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
