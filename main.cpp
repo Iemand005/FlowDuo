c0f113d52ec2fb7f62e4495938aa82172c9e3b54
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-13 03:38:28 +0200

    ok

--- main.cpp
+++ main.cpp
@@ -385,7 +385,7 @@ static void PresentQuad(ID3D11Device* device, ID3D11DeviceContext* context, IDXG
         XMMatrixScaling(scale, scaleY, scale) *
         XMMatrixRotationX(XMConvertToRadians(g_tiltDeg / 2)) *
         XMMatrixTranslation(0.0f, -g_quadHalfHeight, 0.0f) *
-        XMMatrixTranslation(0.0f, -(scale - 1.0f) * g_quadHalfHeight, 0.0f);  // <-- realign to screen bottom
+        XMMatrixTranslation(0.0f, -(scale - 1.0f) * g_quadHalfHeight, 0.0f);
     XMMATRIX view = XMMatrixLookAtLH(XMVectorSet(0.0f, 0.0f, -3.0f, 1.0f),
                                      XMVectorZero(), XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f));
     XMMATRIX proj = XMMatrixPerspectiveFovLH(XMConvertToRadians(60.0f), aspect, 0.1f, 100.0f);
