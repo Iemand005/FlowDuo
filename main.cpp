277226b2e0307957947a2a9728a84e3cf42ea915
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-13 15:26:00 +0200

    faster hinge update

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ static float g_fadeEnd = 1.0f;
 static float g_fadeStrength = 0.0f;
 static float g_blurRadius = 20.0f;
 static float g_blurRadiusMultiplier = 2.0f;
-static DWORD g_hingeSampleIntervalMs = 8;
+static DWORD g_hingeSampleIntervalMs = 1;
 static float g_effectResolutionScale = 0.7f;
 
 static bool calibrated = false;
