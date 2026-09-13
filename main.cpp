1c263f9bc78d249ef232e0de0beb364082bd4a8b
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-13 03:23:48 +0200

    TRH

--- main.cpp
+++ main.cpp
@@ -384,9 +384,9 @@ static void PresentQuad(ID3D11Device* device, ID3D11DeviceContext* context, IDXG
         XMMatrixTranslation(0.0f, g_quadHalfHeight, 0.0f) *   // pivot scale to bottom
         XMMatrixScaling(scale, scaleY, scale) *
         XMMatrixTranslation(0.0f, -g_quadHalfHeight, 0.0f) *
-        XMMatrixTranslation(0.0f, topY, 0.0f) *                // pivot rotation to true top
+        XMMatrixTranslation(0.0f, -topY, 0.0f) *               // move top to origin
         XMMatrixRotationX(XMConvertToRadians(g_tiltDeg / 2)) *
-        XMMatrixTranslation(0.0f, -topY, 0.0f);
+        XMMatrixTranslation(0.0f, topY, 0.0f);                 // move back
     XMMATRIX view = XMMatrixLookAtLH(XMVectorSet(0.0f, 0.0f, -3.0f, 1.0f),
                                      XMVectorZero(), XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f));
     XMMATRIX proj = XMMatrixPerspectiveFovLH(XMConvertToRadians(60.0f), aspect, 0.1f, 100.0f);
